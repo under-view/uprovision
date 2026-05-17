d48be4f317a977f1a0427189accc240d31409dfa
Vincent Davis Jr <vince@underview.tech>
2026-05-17 12:40:00 -0400

    device: remove goto's
    Signed-off-by: Vincent Davis Jr <vince@underview.tech>

--- src/device.c
+++ src/device.c
@@ -26,63 +26,95 @@ struct uprov_device
 	char                          *block_device;
 	unsigned int                  part_count;
 	struct uprov_device_partition *partitions;
-	void                          *fdisk_context;
 };
 
-/**********************************************
- * start uprov_device_create_create functions *
- **********************************************/
+/*****************************************
+ * Start of global to C source functions *
+ *****************************************/
+
+struct p_uprov_fdisk
+{
+	struct fdisk_context *ctx;
+	struct fdisk_table   *table;
+};
+
+
+static void
+p_uprov_fdisk_destroy (struct p_uprov_fdisk *fdisk)
+{
+	if (fdisk->table)
+		fdisk_unref_table(fdisk->table);
+	if (fdisk->ctx) {
+		fdisk_deassign_device(fdisk->ctx, 1);
+		fdisk_unref_context(fdisk->ctx);
+	}
+}
+
+/***************************************
+ * End of global to C source functions *
+ ***************************************/
+
+
+/***************************************
+ * Start uprov_device_create functions *
+ ***************************************/
 
 #define BLOCK_DEVICE_MAX_SIZE 50
 
 static int
 device_create_with_fdisk (struct uprov_device *device)
 {
-	int ret = -1;
+	int err = -1;
 	unsigned int p;
 
-	struct fdisk_context *cxt = NULL;
-	struct fdisk_table *table = NULL;
+	struct p_uprov_fdisk fdisk;
+
 	struct fdisk_partition *part = NULL;
 
-	cxt = fdisk_new_context();
-	if (!cxt) {
+	memset(&fdisk, 0, sizeof(struct p_uprov_fdisk));
+
+	fdisk.ctx = fdisk_new_context();
+	if (!(fdisk.ctx)) {
 		udo_log_error("fdisk_new_context failed\n");
-		goto device_create_with_fdisk_exit;
+		return -1;
 	}
 
-	device->fdisk_context = cxt;
 	device->bdev_fd = open(device->block_device, O_RDWR);
 	if (device->bdev_fd < 0) {
 		udo_log_error("open: %s\n", strerror(errno));
-		goto device_create_with_fdisk_exit;
+		p_uprov_fdisk_destroy(&fdisk);
+		return -1;
 	}
 
-	ret = fdisk_assign_device_by_fd(cxt, device->bdev_fd, device->block_device, 0);
-	if (ret < 0) {
+	err = fdisk_assign_device_by_fd(fdisk.ctx, \
+		device->bdev_fd, device->block_device, 0);
+	if (err < 0) {
 		udo_log_error("fdisk_assign_device_by_fd('%d','%s') failed\n",
 		              device->bdev_fd, device->block_device);
-		goto device_create_with_fdisk_exit;
+		p_uprov_fdisk_destroy(&fdisk);
+		return -1;
 	}
 
-	ret = fdisk_get_partitions(cxt, &table);
-	if (ret != 0) {
+	err = fdisk_get_partitions(fdisk.ctx, &(fdisk.table));
+	if (err != 0) {
 		udo_log_error("fdisk_get_partitions('%d','%s') failed\n",
 		              device->bdev_fd, device->block_device);
-		goto device_create_with_fdisk_exit;
+		p_uprov_fdisk_destroy(&fdisk);
+		return -1;
 	}
 
-	device->block_size = fdisk_get_sector_size(cxt);
-	device->part_count = fdisk_table_get_nents(table);
+	device->block_size = fdisk_get_sector_size(fdisk.ctx);
+	device->part_count = fdisk_table_get_nents(fdisk.table);
 
 	device->partitions = calloc(device->part_count, sizeof(struct uprov_device_partition));
 	if (!device->partitions) {
 		udo_log_error("calloc: %s\n", strerror(errno));
-		goto device_create_with_fdisk_exit;
+		p_uprov_fdisk_destroy(&fdisk);
+		return -1;
 	}
 
 	for (p = 0; p < device->part_count; p++) {
-		part = fdisk_table_get_partition_by_partno(table, p);
+		part = fdisk_table_get_partition_by_partno(fdisk.table, p);
 
 		device->partitions[p].number = fdisk_partition_get_partno(part); // redundant, but fine
 		device->partitions[p].start_sector = fdisk_partition_get_start(part);
@@ -92,12 +124,9 @@ device_create_with_fdisk (struct uprov_device *device)
 		fdisk_unref_partition(part); part = NULL;
 	}
 
-device_create_with_fdisk_exit:
+	p_uprov_fdisk_destroy(&fdisk);
 
-	if (table)
-		fdisk_unref_table(table);
-
-	return ret;
+	return 0;
 }
 
 
@@ -115,25 +144,24 @@ uprov_device_create (struct uprov_device_create_info *device_info)
 	}
 
 	device->block_device = strndup(device_info->block_device, BLOCK_DEVICE_MAX_SIZE);
-	if (!device->block_device) {
+	if (!(device->block_device)) {
 		udo_log_error("strndup: %s\n", strerror(errno));
-		goto uprov_device_create_exit_error;
+		uprov_device_destroy(device);
+		return NULL;
 	}
 
 	ret = device_create_with_fdisk(device);
-	if (ret == -1)
-		goto uprov_device_create_exit_error;
+	if (ret == -1) {
+		uprov_device_destroy(device);
+		return NULL;
+	}
 
 	return device;
-
-uprov_device_create_exit_error:
-	uprov_device_destroy(device);
-	return NULL;
 }
 
-/********************************************
- * End uprov_device_create_create functions *
- ********************************************/
+/*************************************
+ * End uprov_device_create functions *
+ *************************************/
 
 
 /******************************************
@@ -143,59 +171,73 @@ uprov_device_create_exit_error:
 static int
 device_resize (struct uprov_device *device, int part_num)
 {
-	int ret = -1;
+	int err = -1;
 
-	uint64_t total_sectors = 0;
-	uint64_t start_sector = 0;
 	uint64_t end_sector = 0;
+	uint64_t start_sector = 0;
+	uint64_t total_sectors = 0;
 	uint64_t offset_sectors = 0;
 
-	struct fdisk_context *cxt = NULL;
-	struct fdisk_table *table = NULL;
 	struct fdisk_partition *part = NULL;
+
+	struct p_uprov_fdisk fdisk;
 	struct uprov_device_partition partition;
 
-	cxt = device->fdisk_context;
 	partition = device->partitions[part_num];
 
-	fdisk_disable_dialogs(cxt, 1);
+	memset(&fdisk, 0, sizeof(struct p_uprov_fdisk));
 
-	ret = fdisk_get_partitions(cxt, &table);
-	if (ret != 0) {
+	fdisk.ctx = fdisk_new_context();
+	if (!(fdisk.ctx)) {
+		udo_log_error("fdisk_new_context failed\n");
+		return -1;
+	}
+
+	err = fdisk_assign_device_by_fd(fdisk.ctx, \
+		device->bdev_fd, device->block_device, 0);
+	if (err < 0) {
+		udo_log_error("fdisk_assign_device_by_fd('%d','%s') failed\n",
+		              device->bdev_fd, device->block_device);
+		p_uprov_fdisk_destroy(&fdisk);
+		return -1;
+	}
+
+	fdisk_disable_dialogs(fdisk.ctx, 1);
+
+	err = fdisk_get_partitions(fdisk.ctx, &(fdisk.table));
+	if (err != 0) {
 		udo_log_error("fdisk_get_partitions('%d','%d') failed\n",
                               device->bdev_fd, device->block_device);
-		goto device_resize_exit;
+		p_uprov_fdisk_destroy(&fdisk);
+		return -1;
 	}
 
 	start_sector = partition.start_sector;
 	end_sector = partition.end_sector;
-	total_sectors = fdisk_get_nsectors(cxt);
+	total_sectors = fdisk_get_nsectors(fdisk.ctx);
 	offset_sectors = total_sectors - start_sector - 512;
 
 	if (total_sectors > end_sector) {
-		part = fdisk_table_get_partition_by_partno(table, partition.number);
+		part = fdisk_table_get_partition_by_partno(fdisk.table, partition.number);
 
 		fdisk_partition_size_explicit(part, 1);
 		fdisk_partition_set_size(part, offset_sectors);
 		fdisk_partition_end_follow_default(part, 1);
 
-		ret = fdisk_set_partition(cxt, partition.number, part);
-		if (ret != 0) {
+		err = fdisk_set_partition(fdisk.ctx, partition.number, part);
+		if (err != 0) {
 			udo_log_error("fdisk_set_partition('%d','%s') failed\n",
 			              device->bdev_fd, device->block_device);
+			p_uprov_fdisk_destroy(&fdisk);
+			return -1;
 		}
 
 		fdisk_unref_partition(part); part = NULL;
 	}
 
-device_resize_exit:
+	p_uprov_fdisk_destroy(&fdisk);
 
-	if (table)
-		fdisk_unref_table(table);
-
-	fdisk_disable_dialogs(cxt, 0);
-
-	return ret;
+	return 0;
 }
 
 
@@ -258,8 +300,6 @@ uprov_device_destroy (struct uprov_device *device)
 	close(device->bdev_fd);
 	free(device->block_device);
 	free(device->partitions);
-	fdisk_deassign_device((struct fdisk_context*)device->fdisk_context, 0);
-	fdisk_unref_context((struct fdisk_context*)device->fdisk_context);
 	free(device);
 }
 
